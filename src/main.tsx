26181e3b81b9e97523a8e6dc5e0100a5c86f542b
MJ <ckx9798@gmail.com>
2025-07-07 22:16:38 +0900

    Feat: 프로젝트 기본 세팅

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import "./index.css";
+
+import App from "./App.tsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
