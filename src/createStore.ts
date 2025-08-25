30e44cce416f0a224db1161b8621683b4666454c
MJ <ckx9798@gmail.com>
2025-08-25 14:12:59 +0900

    Feat: 객체 병합 문제 해결

--- src/createStore.ts
+++ src/createStore.ts
@@ -2,13 +2,12 @@
 
 export function createStore(initializer) {
   let state;
-
   const getState = () => state;
 
   const setState = (updater) => {
     const newState = typeof updater === "function" ? updater(state) : updater;
 
-    state = newState;
+    state = { ...state, ...newState };
   };
 
   state = typeof initializer === "function" ? initializer() : initializer;
